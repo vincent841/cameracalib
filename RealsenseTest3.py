2a2acbd3e23501779a5015f368168e961bed585f
jinwon <vincent841@naver.com>
2020-05-18 09:17:01 +0900

    * modify test codes for realsense camera

--- RealsenseTest3.py
+++ RealsenseTest3.py
@@ -30,20 +30,13 @@ try:
         depth_to_color_extrin = depth_frame.profile.get_extrinsics_to(
             color_frame.profile)
 
+        print(color_intrin)
+
         # print(depth_intrin.ppx, depth_intrin.ppy)
 
         # Convert images to numpy arrays
         depth_image = np.asanyarray(depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
-
-        # focusing on a (320, 240) pixel
-        depth = depth_frame.get_distance(320, 239)
-        depth_point = rs.rs2_deproject_pixel_to_point(depth_intrin, [320, 239], depth)
-        text = "%.5lf, %.5lf, %.5lf\n" % (depth_point[0], depth_point[1], depth_point[2])
-        #print(text)
-        font = cv2.FONT_HERSHEY_PLAIN
-        cv2.putText(color_image, "[1] 3D Coord:" + text, (0,64), font, 1, (0,255,0),1,cv2.LINE_AA)
-        cv2.rectangle(color_image, (319, 238), (321, 240), (255, 0, 0), 2)
 
         # focusing on a (320, 120) pixel
         depth = depth_frame.get_distance(320, 120)
@@ -51,8 +44,17 @@ try:
         text = "%.5lf, %.5lf, %.5lf\n" % (depth_point[0], depth_point[1], depth_point[2])
         #print(text)
         font = cv2.FONT_HERSHEY_PLAIN
-        cv2.putText(color_image, "[2] 3D Coord:" + text, (0,96), font, 1, (0,255,0),1,cv2.LINE_AA)
-        cv2.rectangle(color_image, (319, 119), (321, 121), (255, 0, 0), 2)        
+        cv2.putText(color_image, "[0] 3D Coord:" + text, (0,64), font, 1, (0,255,0),1,cv2.LINE_AA)
+        cv2.rectangle(color_image, (319, 119), (321, 121), (255, 0, 0), 2)      
+
+        # focusing on a (320, 240) pixel
+        depth = depth_frame.get_distance(320, 239)
+        depth_point = rs.rs2_deproject_pixel_to_point(depth_intrin, [320, 239], depth)
+        text = "%.5lf, %.5lf, %.5lf\n" % (depth_point[0], depth_point[1], depth_point[2])
+        #print(text)
+        font = cv2.FONT_HERSHEY_PLAIN
+        cv2.putText(color_image, "[1] 3D Coord:" + text, (0,96), font, 1, (0,255,0),1,cv2.LINE_AA)
+        cv2.rectangle(color_image, (319, 238), (321, 240), (255, 0, 0), 2)
 
         # focusing on a (320, 360) pixel
         depth = depth_frame.get_distance(320, 360)
